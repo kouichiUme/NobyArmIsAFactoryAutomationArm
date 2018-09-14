9487fcc57004dfaa93183c874a52e9626021c462
kouichiUme <kouichiume@gmail.com>
2018-09-15 02:01:40 +0900

    sample soft pwm with python and pigpio

--- sampleArm.py
+++ sampleArm.py
@@ -2,8 +2,8 @@ import pigpio
 import time
 
 
-gpio_pin0 = 18
-gpio_pin1 = 19
+gpio_pin0 = 17
+gpio_pin1 = 18
 
 
 print("start pigpio");
@@ -12,12 +12,13 @@ pi = pigpio.pi()
 pi.set_mode(gpio_pin0, pigpio.OUTPUT)
 pi.set_mode(gpio_pin1, pigpio.OUTPUT)
 
-pi.hardware_PWM(gpio_pin0,800,255)
-# GPIO19: 8Hz、duty比0.1
-pi.hardware_PWM(gpio_pin1, 800, 100000)
+pi.set_PWM_range(17,800)
+pi.set_PWM_dutycycle(gpio_pin0,255)
+#pi.hardware_PWM(gpio_pin1, 800, 100000)
 
 time.sleep(5)
+pi.set_PWM_dutycycle(gpio_pin0,0)
 
 pi.set_mode(gpio_pin0, pigpio.INPUT)
-pi.set_mode(gpio_pin1, pigpio.INPUT)
+#pi.set_mode(gpio_pin1, pigpio.INPUT)
 pi.stop()
