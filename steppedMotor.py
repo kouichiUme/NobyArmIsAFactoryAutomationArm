40073dd373095cbfc813d778e7b769d26698fdc1
kouichiUme <kouichiume@gmail.com>
2018-10-21 19:02:58 +0900

    ccw rotate

--- steppedMotor.py
+++ steppedMotor.py
@@ -3,22 +3,41 @@
 import pigpio
 import time
 
+# X direction
 
-gpio_pin_orange = 12
-gpio_pin_yellow = 16
-gpio_pin_brown = 20
-gpio_pin_black = 21
+gpio_pin_X_orange = 12
+gpio_pin_X_yellow = 16
+gpio_pin_X_brown = 20
+gpio_pin_X_black = 21
+
+# Y direction (height)
+
+gpio_pin_Y_orange = 15
+gpio_pin_Y_yellow = 25
+gpio_pin_Y_brown = 8
+gpio_pin_Y_black = 7
+
+# Z direction (height)
+
+gpio_pin_Z_orange = 2
+gpio_pin_Z_yellow = 3
+gpio_pin_Z_brown = 4
+gpio_pin_Z_black = 12
 
 
 print("start pigpio")
 
+
 pi = pigpio.pi()
-pi.set_mode(gpio_pin_orange, pigpio.OUTPUT)
-pi.set_mode(gpio_pin_yellow, pigpio.OUTPUT)
-pi.set_mode(gpio_pin_brown, pigpio.OUTPUT)
-pi.set_mode(gpio_pin_black, pigpio.OUTPUT)
+pi.set_mode(gpio_pin_X_orange, pigpio.OUTPUT)
+pi.set_mode(gpio_pin_X_yellow, pigpio.OUTPUT)
+pi.set_mode(gpio_pin_X_brown, pigpio.OUTPUT)
+pi.set_mode(gpio_pin_X_black, pigpio.OUTPUT)
 time.sleep(5)
 
+startCCWRotate(True)
+
+def startCWRotate(startStop):
 # X is black
 # X_ is orange
 # Y is brown
@@ -31,27 +50,30 @@ time.sleep(5)
 # Y     off off on on   off off on
 # Y_    on off off on   on off off
 # clockwise
-while True:
-    pi.write(gpio_pin_black, 1)
-    pi.write(gpio_pin_yellow, 0)
-    pi.write(gpio_pin_orange, 1)
+    i = 0
+    while startStop and i < 30:
+        pi.write(gpio_pin_X_black, 1)
+        pi.write(gpio_pin_X_yellow, 0)
+        pi.write(gpio_pin_X_orange, 1)
 
-    pi.write(gpio_pin_black, 0)
-    pi.write(gpio_pin_brown, 1)
+        pi.write(gpio_pin_X_black, 0)
+        pi.write(gpio_pin_X_brown, 1)
 
-    pi.write(gpio_pin_orange, 0)
-    pi.write(gpio_pin_yellow, 1)
-    pi.write(gpio_pin_brown, 0)
-
-
+        pi.write(gpio_pin_X_orange, 0)
+        pi.write(gpio_pin_X_yellow, 1)
+        pi.write(gpio_pin_X_brown, 0)
+        i += 1
+#
+def startCCWRotate(startStop):
 # counter clock wise
-#    pi.write(gpio_pin_black,1)
-#    pi.write(gpio_pin_yellow,0)
-#    pi.write(gpio_pin_orange,1)
-
-# pi.write(gpio_pin_black,0)
-# pi.write(gpio_pin_brown,1)
-
-# pi.write(gpio_pin_orange,0)
-# pi.write(gpio_pin_yellow,1)
-# pi.write(gpio_pin_brown,0)
+    i = 0
+    while startStop and i < 30:
+        pi.write(gpio_pin_X_brown,0)
+        pi.write(gpio_pin_X_yellow,1)
+        pi.write(gpio_pin_X_orange,0)
+        pi.write(gpio_pin_X_brown,1)
+        pi.write(gpio_pin_X_black,0)
+        pi.write(gpio_pin_X_orange,1)
+        pi.write(gpio_pin_X_yellow,0)
+        pi.write(gpio_pin_X_black,1)
+        i+=1
